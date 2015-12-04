40ac536a3f08d09b0436fda355be0d7b45346972
Fredrik Johansson <c04fjn@cs.umu.se>
2015-12-04 11:37:48 +0100

    Aded SenseData endPoint for server side and controllers for client side together with corresponding html, js and css files

--- server/config/socketio.js
+++ server/config/socketio.js
@@ -18,6 +18,7 @@ function onConnect(socket) {
   });
 
   // Insert sockets below
+  require('../api/sensedata/sensedata.socket').register(socket);
   require('../api/thing/thing.socket').register(socket);
 }
 
